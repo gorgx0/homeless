89959d2fecfb5173e88aad83c1251bc79c754203
Marcin Krasowski <marcin.krasowski@gmail.com>
2020-01-21 00:40:26 +0100

    bpm and webpack working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import L from 'leaflet';
 import KML from 'leaflet-kml';
+import 'leaflet/dist/leaflet.css';
+import './index.css'
+
+let map = document.createElement('div');
+map.id = "mapid";
+document.body.appendChild(map);
 
 var mymap = L.map('mapid').setView([52.17, 21.0], 15);
+console.log(map);
+console.log(mymap);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
-    entry: './src/index.js',
+    entry:{
+        app: './src/index.js',
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname,'dist')
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: "Homeless map"
-        })
+        }),
+        new CopyPlugin([
+            {from: 'data', to: 'data'},
+            {from: 'images', to: 'images'}
+        ])
     ],
     module: {
         rules: [
@@ -20,7 +30,17 @@ module.exports = {
                     'style-loader',
                     'css-loader'
                 ],
-            },
+            },{
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }
         ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 9000
+    },
+    node: {
+        fs: 'empty'
     }
 };
