8e1e7481b95a7f6c0b87bf286f40162498a1707c
Marcin Krasowski <marcin.krasowski@gmail.com>
2020-01-22 01:58:06 +0100

    geo-backend added

---
+++ geo-backend/src/main/java/com/gorg/App.java
@@ -0,0 +1,13 @@
+package com.gorg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
