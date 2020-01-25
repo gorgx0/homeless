6075ce190383fdde7998988b4cfed7048edcd3bf
Marcin Krasowski <marcin.krasowski@gmail.com>
2020-01-25 22:04:53 +0100

    new module

---
+++ homeless-backend/src/test/java/com/gorg/homeless/HomelessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorg.homeless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomelessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
